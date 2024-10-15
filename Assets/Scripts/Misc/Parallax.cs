971e8bfb309ca9c8c3b6bc6aae18ed00471275ab
Dede44373 <ysd4417@autuni.ac.nz>
2024-10-15 22:49:15 +1300

    15/10/2024
    added enemy attack animations, ranged idle animations, and added idle checkpoint animation. Also added background parallax.

---
+++ Assets/Scripts/Misc/Parallax.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [Header("Boss door shennanigans")]
+    public Door door;
+    [SerializeField] float speed;
+    private Vector3 velocity = Vector3.zero;
+
+    [Header ("Parallax things")] 
+    private float length, startPos;
+    public GameObject cam;
+    public float parallaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (temp < startPos - length) 
+        { 
+            startPos -= length;   
+        }
+
+        if (door.atBoss == true)
+        {
+            transform.localScale = Vector3.SmoothDamp(transform.localScale, new Vector3(1.5f, 1.5f, transform.localScale.z), ref velocity, speed);
+           
+        }
+
+    }
+    
+
+}
